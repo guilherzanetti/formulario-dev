82177cdcafbe3d9b3300cdb318bfd8cf6b66b53b
HeitorRafael <hitoor.d@gmail.com>
2023-11-19 19:28:09 -0300

    ok

---
+++ src/result/changeInfo.js
@@ -0,0 +1,25 @@
+//variáveis
+const programerImage = document.getElementById("programerimage");
+const image1 = document.getElementById("image1");
+const image2 = document.getElementById("image2");
+const image3 = document.getElementById("image3");
+
+const programerDescription = document.getElementById("programerDescription");
+const description1 = document.getElementById("description1");
+const description2 = document.getElementById("description2");
+const description3 = document.getElementById("description3");
+
+//algoritmo
+
+
+//seleção de img e texto
+if (resultAlg = x) {
+    programerImage = image1;
+    programerDescription = description1.innerText
+} else if (resultAlg = y) {
+    programerImage = image2;
+    programerDescription = description2.innerText
+} else {
+    programerImage = image3;
+    programerDescription = description3.innerText
+}
