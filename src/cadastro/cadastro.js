f774f221f60eb94a7e065b567f083c5dcee114a1
HeitorRafael <hitoor.d@gmail.com>
2023-11-26 17:53:06 -0300

    arrumando

--- src/cadastro/cadastro.js
+++ src/cadastro/cadastro.js
@@ -7,7 +7,7 @@ const perguntas = document.querySelectorAll('.perguntas');
 function selectLabel(label, perguntaIndex) {
   const perguntas = document.querySelectorAll('.perguntas');
   const labels = perguntas[perguntaIndex].querySelectorAll('label');
-  
+
   labels.forEach((l) => {
       l.classList.remove('selectedLabel');
   });
@@ -41,32 +41,3 @@ btnCadastrar.addEventListener("click", (e) => {
       )
     : alert("Por favor, insira um nome válido.");
 });
-
-
-//                                                    tratando respostas
-
-// função para verificar respostas
-
-// function selectedRadio() {
-//   const questions = document.querySelectorAll('.perguntas');
-
-  questions.forEach(function (question) {
-    const inputClient = question.querySelectorAll('input[type="radio"]');
-    const selectedInputClient = Array.from(inputClient).find(input => input.checked);
-    
-    console.log(selectedInputClient ? selectedInputClient.value : null)
-  });
-}
-
-document.getElementById("btnCadastrar").addEventListener('click', selectedRadio);
-//     questions.forEach(function name(questions) {
-
-//       const inputClient = questions.querySelectorAll ('input[type="radio"]');
-
-//       const selectedInputClient = Array.from(inputClient).find(inputClient => inputClient.checked);
-      
-//     console.log(selectedInputClient ? selectedInputClient.value : null);
-//   });
-// };
-// document.getElementById("btnCadastrar").addEventListener('click', selectedRadio);
-
