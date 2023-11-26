34b7714869c30059a29d99c449acafd73cc10aab
Guilherme Zanetti <94937630+guilherzanetti@users.noreply.github.com>
2023-11-26 20:21:28 -0300

    CadastroQuasePronto

--- src/cadastro/cadastro.js
+++ src/cadastro/cadastro.js
@@ -19,7 +19,7 @@ function selectLabel(label, perguntaIndex) {
 // Backend-Backend-Backend-Backend-Backend-Backend-Backend-Backend-Backend-Backend-Backend-Backend-Backend-
 
 
-//                                                 Tratando nomes
+
 //Variáveis
 const nome = document.getElementById("nome");
 const btnCadastrar = document.getElementById("btnCadastrar");
@@ -42,22 +42,155 @@ btnCadastrar.addEventListener("click", (e) => {
     : alert("Por favor, insira um nome válido.");
 });
 
+// CADASTRO
 
-//                                                    tratando respostas
+let devImg, devDesc, devCaracter;
 
-// função para verificar respostas
+        // Adicione esta função ao seu arquivo cadastro.js
+        function obterRespostasConsole() {
+            const nome = document.querySelector('#nome').value;
+            const respostas = {
+                nome: nome,
+                genero: obterRespostaPergunta('genero'),
+                q1: obterRespostaPergunta('q1'),
+                q2: obterRespostaPergunta('q2'),
+                q3: obterRespostaPergunta('q3'),
+                q4: obterRespostaPergunta('q4'),
+                q5: obterRespostaPergunta('q5'),
+                // Adicione mais perguntas conforme necessário
+            };
 
-// function selectedRadio() {
-//   const questions = document.querySelectorAll('.perguntas');
+            const q1resposta = respostas.q1
+            const q2resposta = respostas.q2
+            const q3resposta = respostas.q3
+            const q4resposta = respostas.q4
+            const q5resposta = respostas.q5
 
-//     questions.forEach(function name(questions) {
+            let resposta1 = 0;
+            let resposta2 = 0;
+            let resposta3 = 0;
 
-//       const inputClient = questions.querySelectorAll ('input[type="radio"]');
+            // Verificação da quantidade da mesma resposta
+            if (q1resposta == "Opção 1") {
+                resposta1 = resposta1 + 1;
+            } else if (q2resposta == "Opção 2") {
+                resposta2 = resposta2 + 1;
+            } else {
+                resposta3 = resposta3 + 1;
 
-//       const selectedInputClient = Array.from(inputClient).find(inputClient => inputClient.checked);
-      
-//     console.log(selectedInputClient ? selectedInputClient.value : null);
-//   });
-// };
-// document.getElementById("btnCadastrar").addEventListener('click', selectedRadio);
+            }
+
+            // Verificar tipo de dev
+
+
+            // const devImg = "";
+            // const devDesc = "";
+            // const devCaracter = "";
+
+            // Verificar qual possui mais
+
+            
+
+            if (resposta1 > resposta2 && resposta1 > resposta3) {
+                // window.location.href = '/src/result/programerType1.html';
+                if (respostas.genero == "masculino") {
+                    devImg = caracterUserMasculino[0].img;
+                    devDesc = caracterUserMasculino[0].descricao;
+                    devCaracter = caracterUserMasculino[0].caracteristica
+                } else {
+                    devImg = caracterUserFeminino[0].img;
+                    devDesc = caracterUserFeminino[0].descricao;
+                    devCaracter = caracterUserFeminino[0].caracteristica
+                }
+
+            } else if (resposta2 > resposta1 && resposta2 > resposta3) {
+                // window.location.href = '/src/result/programerType2.html';
+                if (respostas.genero == "masculino") {
+                    devImg = caracterUserMasculino[1].img;
+                    devDesc = caracterUserMasculino[1].descricao;
+                    devCaracter = caracterUserMasculino[1].caracteristica
+                } else {
+                    devImg = caracterUserFeminino[1].img;
+                    devDesc = caracterUserFeminino[1].descricao;
+                    devCaracter = caracterUserFeminino[1].caracteristica
+                }
+            } else {
+                // window.location.href = '/src/result/programerType3.html';
+                if (respostas.genero == "masculino") {
+                    devImg = caracterUserMasculino[2].img;
+                    devDesc = caracterUserMasculino[2].descricao;
+                    devCaracter = caracterUserMasculino[2].caracteristica
+                } else {
+                    devImg = caracterUserFeminino[2].img;
+                    devDesc = caracterUserFeminino[2].descricao;
+                    devCaracter = caracterUserFeminino[2].caracteristica
+                }
+            }
+            console.log("Você é um dev:", devCaracter);
+            console.log(resposta1, resposta2, resposta3);
+            console.log(respostas);
+            console.log("a resposta q1 é ", q1resposta)
+        }
+
+
+        // Altere o evento associado ao botão "Cadastrar" para chamar a nova função
+        document.getElementById("btnCadastrar").addEventListener("click", obterRespostasConsole);
+
+        // Mantenha a função obterRespostaPergunta no seu arquivo cadastro.js
+        function obterRespostaPergunta(nomePergunta) {
+            const opcoes = document.querySelectorAll(`input[name="${nomePergunta}"]:checked`);
+
+            if (opcoes.length > 0) {
+                return opcoes[0].value;
+            }
+
+            return null; // Retorna null se nenhuma opção estiver selecionada
+        }
+
+        const caracterUserMasculino = [
+            {
+                caracteristica: "Preguiçoso",
+                descricao: "Você tem preguiça",
+                img: "url"
+
+            },
+
+            {
+                caracteristica: "Monge",
+                descricao: "lorem",
+                img: "url"
+
+            },
+
+            {
+                caracteristica: "Louco",
+                descricao: "lorem",
+                img: "url"
+
+            }
+        ];
+
+        const caracterUserFeminino = [
+            {
+                caracteristica: "Preguiçosa",
+                descricao: "Você tem preguiça",
+                img: "url"
+
+            },
+
+            {
+                caracteristica: "Monge",
+                descricao: "lorem",
+                img: "url"
+
+            },
+
+            {
+                caracteristica: "Louca",
+                descricao: "lorem",
+                img: "url"
+
+            }
+        ];
+
 
