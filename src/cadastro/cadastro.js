1ff2d5cdb112d80a4301083f90ebfc26b72d5900
Guilherme Zanetti <94937630+guilherzanetti@users.noreply.github.com>
2023-11-26 17:56:06 -0300

    TesteBranch

--- src/cadastro/cadastro.js
+++ src/cadastro/cadastro.js
@@ -42,22 +42,3 @@ btnCadastrar.addEventListener("click", (e) => {
     : alert("Por favor, insira um nome válido.");
 });
 
-
-//                                                    tratando respostas
-
-// função para verificar respostas
-
-// function selectedRadio() {
-//   const questions = document.querySelectorAll('.perguntas');
-
-//     questions.forEach(function name(questions) {
-
-//       const inputClient = questions.querySelectorAll ('input[type="radio"]');
-
-//       const selectedInputClient = Array.from(inputClient).find(inputClient => inputClient.checked);
-      
-//     console.log(selectedInputClient ? selectedInputClient.value : null);
-//   });
-// };
-// document.getElementById("btnCadastrar").addEventListener('click', selectedRadio);
-
