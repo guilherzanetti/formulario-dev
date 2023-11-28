a1310168fb38086820e3b5f53e95e8d164653d6b
HeitorRafael <hitoor.d@gmail.com>
2023-11-28 10:15:26 -0300

    esta funcionando parcialmente a adição no select

--- procurardev.js
+++ procurardev.js
@@ -2,31 +2,16 @@
 const selectUser = document.getElementById ("selectUser")
 
 var client = localStorage.getItem("arrayReposta"); 
-
-if (client) {
-    var client = JSON.parse(client);
-    var nomeDoObjt = client[0].nome;
-
-  function adUser(client) {
-  var client = localStorage.getItem("arrayReposta");  
-    //faz criar um campo de option no documento
-    const option = document.createElement("option");
-    
-    //declara que o texto e o valor da opção serão virão do parametro resposta
-    option.text = client;
-    option.value = client;
-
-    //adiciona a opção ao campo do menu suspenso
-    selectUser.appendChild(option);
-    }  
-};
+var client = JSON.parse(client);
 console.log(client[0].nome);
 
-//func. de adicionar usuario as opções na pag.
+if (client) {
+    client.forEach(function (client) {
+        const option = document.createElement("option");
 
+        option.text = client.nome
+        option.value = client.nome
+        selectUser.appendChild(option);
+    });
+};
 
-
-//faz um loopin para verificar o indice no arrayNames do nome adicionado e chama a função de adicionar usuario
-for (var i = 0; i < arrayResposta.length; i++) {
-    adUser(arrayResposta[i]);
-  }
